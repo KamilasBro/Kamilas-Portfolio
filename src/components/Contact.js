e9be31c9cbdf15450211e3d585f5eae4d68b71aa
siema <mrbrogopro@gmail.com>
2023-05-20 17:18:04 +0200

    commitment text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ export default function Contact() {
     return (
         <section className="contact">
             <h1 className="section-title">Get in touch</h1>
-            <form className="contact-form" name="contact" method="post" data-netlify-honeypot="bot-field" action={alert("form sended")}>
+            <form className="contact-form" name="contact" method="post" data-netlify-honeypot="bot-field" action={console.log("form sended")}>
             <input type="hidden" name="form-name" value="contact" />
             <div hidden><input name="bot-field"/></div>
                 <div className="contact-inputs">
