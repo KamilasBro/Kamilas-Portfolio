de3d039b1041fab182f6a3ff4a42d826495f5dca
siema <mrbrogopro@gmail.com>
2023-05-20 17:07:29 +0200

    commitment text

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,6 +9,24 @@ export default function Contact() {
         setFocusInput(null)
         document.removeEventListener("click",handleClick, true)
     }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+      
+        const myForm = event.target;
+        const formData = new FormData(myForm);
+        
+        fetch("/", {
+          method: "POST",
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          body: new URLSearchParams(formData).toString(),
+        })
+          .then(() => alert("Thank you for your submission"))
+          .catch((error) => alert(error));
+      };
+      
+      document
+        .querySelector(".contact-form")
+        .addEventListener("submit", handleSubmit);
     return (
         <section className="contact">
             <h1 className="section-title">Get in touch</h1>
